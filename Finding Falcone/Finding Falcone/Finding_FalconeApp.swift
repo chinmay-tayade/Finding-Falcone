06b936e14173112f85a20384b2d83c7f4394809a
Chinmay Tayade <97028462+chinmayindepay@users.noreply.github.com>
2023-09-04 00:13:40 +0530

    theme changes

--- Finding Falcone/Finding Falcone/Finding_FalconeApp.swift
+++ Finding Falcone/Finding Falcone/Finding_FalconeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Finding_FalconeApp: App {
     var body: some Scene {
         WindowGroup {
-            OnboardingView()
+            OnboardingView().preferredColorScheme(.light)
         }
     }
 }
